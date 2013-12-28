90810c6df6d6ee5f909eb68886b37ab0bd09824a
xixisidi <xixisidi@gmail.com>
2013-12-28 14:43:36 +0800

    First commit.

---
+++ test1.py
@@ -0,0 +1,18 @@
+#-*- coding:utf-8 -*-
+
+import sys
+import os
+
+def main():
+    sys.stdout.write("开始程序\n")
+    i=10
+    j='老梅花'
+    print str(i)+j
+    dir= os.listdir('/opt')
+    print type(dir)
+    for s in dir:
+        print s+'\r'
+    
+if __name__ == '__main__':
+    main()
+
